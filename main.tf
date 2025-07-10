8d6bee3e9c27e6e290789747d43ab32f33f1c0d6
SeshaSai-S <seshasai1003@gmail.com>
2025-07-10 17:04:52 +0530

    added subnet seshasn01

--- main.tf
+++ main.tf
@@ -20,4 +20,9 @@ resource "azurerm_virtual_network" "vn-name" {
   location = azurerm_resource_group.rg-name.location
   address_space = ["10.0.0.0/16"]  
 }
-  
+resource "azurerm_subnet" "namesn01" {
+  name = "seshasn01"
+  resource_group_name = azurerm_resource_group.rg-name.name
+  virtual_network_name = azurerm_virtual_network.vn-name.name
+  address_prefixes = ["10.0.1.0/24"]  
+}
