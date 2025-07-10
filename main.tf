7d520edb22e27f0b8b566afdb765148493839395
SeshaSai-S <seshasai1003@gmail.com>
2025-07-10 09:26:07 +0530

    virtual network code

--- main.tf
+++ main.tf
@@ -9,4 +9,14 @@ terraform {
 provider "azurerm" {
   features{}
 }
+resource "azurerm_resource_group" "rg-name" {
+  name = "devops-practise"
+  location = "UK West"  
+}
+resource "azurerm_virtual_network" "vn-name" {
+  name = " sesha-vn"
+  resource_group_name = azurerm_resource_group.rg-name
+  location = azurerm_resource_group.location
+  address_space = ["10.0.0.1/16"]  
+}
   
