9a6b579eff338980230e85190ce48f54cbaec8ec
SeshaSai-S <seshasai1003@gmail.com>
2025-07-11 18:28:21 +0530

    fixed the error

--- vmvariables-examples/variables.tf
+++ vmvariables-examples/variables.tf
@@ -1,36 +1,36 @@
 variable "rg_name" {
-    type = "String"
+    type = "string"
     description = "Name of the resource group"  
 }
 variable "rg_location" {
-    type = "String"
+    type = "string"
     description = "resource group location"  
 }
 variable "vn_name" {
-    type = "String"
+    type = "string"
     description = "Name of the virtualnetwork"
 }
 variable "sn_name" {
-    type = "String"
+    type = "string"
     description = "subnet name"
 }
 variable "vm_name" {
-    type ="String"
+    type = "string"
     description = "Name of the virtual machine"
 }
 variable "nic_name" {
-    type ="String"
+    type = "string"
     description = "Name of the virtual machine"
 }
 variable "pip_name" {
-    type ="String"
+    type = "string"
     description = "Name of the virtual machine"
 }
 variable "nsg_name" {
-    type ="String"
+    type = "string"
     description = "Name of the virtual machine"
 }
 variable "nsr_name" {
-    type ="String"
+    type = "string"
     description = "Name of the virtual machine"
 }
