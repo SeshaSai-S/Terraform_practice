5992f963e84749d2b779cb1ae9d4c2ee0ef70059
SeshaSai-S <seshasai1003@gmail.com>
2025-07-13 09:56:56 +0530

    sdfggjhkk

--- Modules-Example/env/prod/prod.tf
+++ Modules-Example/env/prod/prod.tf
@@ -5,6 +5,13 @@ terraform {
       version = "=4.35.0"
     }
   }
+  backend "azurerm" {
+    resource_group_name = "devops-practise"
+    storage_account_name = "seshasac15"
+    container_name = "terraform"
+    key = "PROD/prod.terraform.tfstate"
+    subscription_id = "62734f71-99dc-45a0-aaeb-dcd7d04d41b7"    
+  }
 }
 provider "azurerm" {
     features {}
@@ -12,12 +19,12 @@ provider "azurerm" {
 }
 module "dev" {
     source = "../../module"
-    rg_name = "PRODRG12"
+    rg_name = "PRODRG123"
     rg_location = "UK West"
-    vn_name = "PRODVN01"
-    sn_name = "PRODSN01"
-    AKSName = "PRODAKS01"
-    acr    = "PRODACR01"
+    vn_name = "PRODVN013"
+    sn_name = "PRODSN013"
+    AKSName = "PRODAKS013"
+    acr    = "PRODACR013"
     address_space = [ "10.0.0.0/16" ]
     address_prefixes = [ "10.0.1.0/24" ]
 }
